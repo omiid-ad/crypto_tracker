2946ad8499154dd9e20e92163298c87d3c1cb9ca
Omid Adibfar <dyarwebpc5@gmail.com>
2025-08-22 15:41:03 +0330

    migration: + Create model `Coin`

--- crypto/models.py
+++ crypto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+    symbol = models.CharField(max_length=20, unique=True, blank=False, null=False)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    last_price_update = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.symbol
