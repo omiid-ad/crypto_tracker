4d703650e7f67369b0cf33bc296e4ade781ac3e8
Omid Adibfar <dyarwebpc5@gmail.com>
2025-08-23 19:35:01 +0330

    migration: ~ Alter field price on coin + Create model CoinPriceHistory

--- crypto/models.py
+++ crypto/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Coin(models.Model):
     symbol = models.CharField(max_length=20, unique=True, blank=False, null=False)
-    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=8, default=0)
     last_price_update = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.symbol
+
+
+class CoinPriceHistory(models.Model):
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE, related_name="price_history")
+    price = models.DecimalField(max_digits=20, decimal_places=8)
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.coin.symbol} - {self.price} @ {self.timestamp}"
