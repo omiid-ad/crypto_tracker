01e5df16099ae0d28cf2c2bc9bcf1d3a69b35ed0
Omid Adibfar <dyarwebpc5@gmail.com>
2025-08-23 20:50:29 +0330

    migration: + Add field english_name to coin + Add field persian_name to coin

--- crypto/models.py
+++ crypto/models.py
@@ -3,12 +3,14 @@ from django.utils import timezone
 
 
 class Coin(models.Model):
+    english_name = models.CharField(max_length=255, blank=True, null=True)
+    persian_name = models.CharField(max_length=255, blank=True, null=True)
     symbol = models.CharField(max_length=20, unique=True, blank=False, null=False)
     price = models.DecimalField(max_digits=20, decimal_places=8, default=0)
     last_price_update = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.symbol
+        return f"{self.symbol} - {self.persian_name or 'No Persian Name'}"
 
 
 class CoinPriceHistory(models.Model):
