4fdbec32164f946c7013b1fbc3552753aa0fac88
Omid Adibfar <dyarwebpc5@gmail.com>
2025-08-23 20:16:16 +0330

    add history endpoint

--- crypto/serializers.py
+++ crypto/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from crypto.models import Coin
+from crypto.models import Coin, CoinPriceHistory
+
+
+class CoinPriceHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CoinPriceHistory
+        fields = ['price', 'timestamp']
 
 
 class CoinSerializer(serializers.ModelSerializer):
--- crypto/views.py
+++ crypto/views.py
@@ -1,13 +1,24 @@
-from rest_framework import viewsets
-from rest_framework.exceptions import NotFound
+from datetime import timedelta
 
-from crypto.models import Coin
+from django.db.models import Prefetch
+from django.utils.timezone import now
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+
+from crypto.models import Coin, CoinPriceHistory
 from crypto.paginations import CoinPagination
-from crypto.serializers import CoinSerializer
+from crypto.serializers import CoinSerializer, CoinPriceHistorySerializer
 
 
 class CoinViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Coin.objects.all()
+    queryset = Coin.objects.prefetch_related(
+        Prefetch(
+            'price_history',
+            queryset=CoinPriceHistory.objects.order_by('-timestamp')
+        )
+    )
     serializer_class = CoinSerializer
     pagination_class = CoinPagination
 
@@ -19,3 +30,15 @@ class CoinViewSet(viewsets.ReadOnlyModelViewSet):
             return self.queryset.get(symbol__iexact=lookup_value)
         except Coin.DoesNotExist:
             raise NotFound(detail="Coin not found")
+
+    @action(detail=True, methods=['get'], url_path='history')
+    def history(self, request, pk=None):
+        coin = self.get_object()
+        three_days_ago = now() - timedelta(days=3)
+        history_qs = [h for h in coin.price_history.all() if h.timestamp >= three_days_ago]
+
+        if not history_qs:
+            raise NotFound("No history found for this coin in the last 3 days.")
+
+        serializer = CoinPriceHistorySerializer(history_qs, many=True)
+        return Response(serializer.data)
